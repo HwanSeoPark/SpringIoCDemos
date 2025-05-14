4bf3debc485a769edc49eb206355d87338704838
HwanSeoPark <qkrghkstj691@gmail.com>
2025-05-14 09:43:06 +0900

    20250514 added DI, Circular Dependency, lazy

---
+++ src/main/java/com/pppspringioc/ioc/dependencies/lazy/model/ExpensiveToCreateBean.java
@@ -0,0 +1,8 @@
+package com.pppspringioc.ioc.dependencies.lazy.model;
+
+public class ExpensiveToCreateBean {
+
+	public ExpensiveToCreateBean() {
+        System.out.println("ExpensiveToCreateBean initialized");
+    }
+}
